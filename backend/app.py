370289ef907ecc3646f60d3fbb36261ca1225c05
toan0309 <46134381+toan0309@users.noreply.github.com>
2026-07-07 09:42:03 +0700

    Xong demo BackEnd và FrontEnd (chưa hoàn thiện)

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,7 @@ import smtplib
 from email.mime.text import MIMEText
 from datetime import datetime, timedelta
 # (password hashing disabled for testing)
+# pyrefly: ignore [missing-import]
 from dotenv import load_dotenv
 
 load_dotenv()
